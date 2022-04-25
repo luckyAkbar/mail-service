93c3d0424d64a23bb5bdfc1a122d73cee2eec537
luckyAkbar <luckyakbar1509@gmail.com>
2022-04-25 07:51:06 +0700

    update Mailing List table attribute

--- internal/models/mailingList.go
+++ internal/models/mailingList.go
@@ -5,12 +5,14 @@ import "gorm.io/gorm"
 type MailingList struct {
 	gorm.Model
 
-	Receipient string `json:"receipient" gorm:"not null"`
-	MailerName string `json:"mailerName" gorm:"not null"`
-	Payload    string `json:"payload" gorm:"not null"`
-	Priority   int    `json:"priority" gorm:"default: 0"`
-	Status     int    `json:"status" gorm:"default: 1"` // default is pending
-	UserID     int
+	Subject         string `json:"subject" gorm:"not null"`
+	ReceipientEmail string `json:"receipient_email" gorm:"not null"`
+	ReceipientName  string `json:"receipient_name" gorm:"not null"`
+	SenderName      string `json:"mailer_name" gorm:"not null"`
+	HtmlContent     string `json:"html_content" gorm:"not null"`
+	Priority        int    `json:"priority" gorm:"default: 0"`
+	Status          int    `json:"status" gorm:"default: 1"` // default is pending
+	UserID          int
 
 	User User `gorm:"foreignKey:UserID"`
 }
