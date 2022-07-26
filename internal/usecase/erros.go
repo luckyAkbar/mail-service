dce8dae6a16a5f701fd785de5d350d0fdd198f6d
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-07-26 20:20:17 +0700

    remove unused

--- internal/usecase/erros.go
+++ internal/usecase/erros.go
@@ -1,17 +1,10 @@
 package usecase
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
+	"errors"
 )
 
 var (
-	ErrSenderNameEmpty       = echo.NewHTTPError(http.StatusBadRequest, "Operation invalid because: sender name is empty")
-	ErrSubjectEmpty          = echo.NewHTTPError(http.StatusBadRequest, "Operation invalid because: subject is empty")
-	ErrHTMLContentEmpty      = echo.NewHTTPError(http.StatusBadRequest, "Operation invalid because: HTML content is empty")
-	ErrMailAdressInvalid     = echo.NewHTTPError(http.StatusBadRequest, "Operation invalid because: email address is invalid")
-	ErrMailAdressAlreadyUsed = echo.NewHTTPError(http.StatusBadRequest, "Operation invalid because: email address is used")
-	ErrPasswordMismatch      = echo.NewHTTPError(http.StatusBadRequest, "Password mismatch")
-	ErrPasswordTooWeak       = echo.NewHTTPError(http.StatusBadRequest, "Password should atleast 8 chars long, with 1 uppercase and 1 special character")
+	ErrValidation = errors.New("validation error")
+	ErrInternal   = errors.New("internal error")
 )
